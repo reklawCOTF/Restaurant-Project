2d248fcc3be76769af9c6a0c4c06e4e8f77a8e24
Trevor <reklawCOTF@gmail.com>
2014-10-13 20:10:15 -0400

    Some more files

---
+++ ClockTest.h
@@ -0,0 +1,53 @@
+#include <ctime>
+#include <iostream>
+#ifndef CLOCKTEST_CPP
+
+using namespace std;
+
+class TimeClock
+{
+public:
+
+    TimeClock()
+        {
+
+        }
+
+    void clockIn()
+    {
+        time_t timeIn = time(NULL); // http://en.cppreference.com/w/cpp/chrono/c/strftime02
+        intTimeIn = timeIn;
+        char mbstr[100];
+        if (strftime(mbstr, sizeof(mbstr), "%A %c", localtime(&timeIn)))
+        {
+            cout << mbstr << endl;;
+        }
+
+    }
+
+    void clockOut()
+    {
+        time_t timeOut = time(NULL); // http://en.cppreference.com/w/cpp/chrono/c/strftime02
+        intTimeOut = timeOut;
+        char mbstr[100];
+        if (strftime(mbstr, sizeof(mbstr), "%A %c", localtime(&timeOut)))
+        {
+            cout << mbstr << endl;
+        }
+        timeWorked = intTimeOut - intTimeIn;
+        cout << timeWorked << endl;
+
+
+    }
+
+
+private:
+time_t timeIn;
+int intTimeIn;
+time_t timeOut;
+int intTimeOut;
+int timeWorked;
+
+};
+
+#endif
