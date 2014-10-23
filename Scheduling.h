e8b5d7d3e71806751755e56cbe98553c493a209f
Robert-Gunther <treygunther@gmail.com>
2014-10-23 12:55:32 -0400

    Update Scheduling.h

--- Scheduling.h
+++ Scheduling.h
@@ -6,10 +6,15 @@
 class schedule
 {
 public:///these require a username
+    void newschedule();
     void newschedule(std::string);
+    void addworkday();
     void addworkday(std::string, std::string);
+    void clrschedule();
     void clrschedule(std::string);
+    void viewschedule();
     void viewschedule(std::string);
+    void delschedule();
     void delschedule(std::string);
     std::string returnschedule(std::string);
 private:
